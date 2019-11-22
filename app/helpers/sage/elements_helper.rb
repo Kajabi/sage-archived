f2a5d1154d71ee44409edf7d8d92c2c8bd81f2b4
cmcfadzean <court@kajabi.com>
2019-11-22 14:50:01 -0800

    wiring up all example code

---
+++ app/helpers/sage/elements_helper.rb
@@ -0,0 +1,9 @@
+module Sage
+  module ElementsHelper
+    def sage_elements
+      [
+        { title: "Button", path: "button" },
+      ]
+    end
+  end
+end
---
+++ app/helpers/sage/modules_helper.rb
@@ -0,0 +1,9 @@
+module Sage
+  module ModulesHelper
+    def sage_modules
+      [
+        { title: "Assistant", path: "assistant" },
+      ]
+    end
+  end
+end
