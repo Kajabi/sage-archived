578ca7a763abfabe5699e77da6afe84c84c1def8
cmcfadzean <court@kajabi.com>
2019-11-22 17:29:47 -0800

    formatting modules

--- app/helpers/sage/modules_helper.rb
+++ app/helpers/sage/modules_helper.rb
@@ -3,6 +3,10 @@ module Sage
     def sage_modules
       [
         { title: "Assistant", path: "assistant" },
+        { title: "Content", path: "content" },
+        { title: "Nav", path: "nav" },
+        { title: "Page", path: "page" },
+        { title: "Sidebar", path: "sidebar" },
       ]
     end
   end
