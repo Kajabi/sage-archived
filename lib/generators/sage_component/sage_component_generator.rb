6fbb017bd1fa70a2b698cb3c147ecd0bcf2ba88b
cmcfadzean <court@kajabi.com>
2020-01-17 21:39:28 -0800

    nesting

--- lib/generators/sage_component/sage_component_generator.rb
+++ lib/generators/sage_component/sage_component_generator.rb
@@ -2,6 +2,6 @@ class SageComponentGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates', __FILE__)
   def create_sage_component
     create_file "app/views/example_components/#{file_name}/#{file_name}.html.erb", "<div class=\"sage-#{file_name}\">\n\n</div>"
-    create_file "app/assets/stylesheets/sage/patterns/components/_#{file_name}.scss", "/*------------------------------------*/\n/*-- #{file_name} component */\n/*------------------------------------*/\n\n.sage-#{file_name} {\n\n  /* #{file_name} elements */\n  &__element { }\n\n  /* #{file_name} modifiers */\n  &--modifier { }\n\n  /* #{file_name} responsive styles */\n  @media (max-width: sage-breakpoint()) {\n    \n  }\n\n}"
+    create_file "app/assets/stylesheets/sage/patterns/components/_#{file_name}.scss", "/*------------------------------------*/\n/*-- #{file_name} component */\n/*------------------------------------*/\n\n.sage-#{file_name} {\n\n  /* #{file_name} elements */\n  &__element { }\n\n  /* #{file_name} modifiers */\n  &--modifier { }\n\n  /* #{file_name} responsive styles */\n  @media (max-width: sage-breakpoint()) {\n    \n  }\n}"
   end
 end
