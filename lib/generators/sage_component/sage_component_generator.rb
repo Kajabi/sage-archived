512c7a8ee2c68e91e76a904e172ea60450847d75
cmcfadzean <court@kajabi.com>
2020-01-21 14:15:07 -0800

    simplifying markup

--- lib/generators/sage_component/sage_component_generator.rb
+++ lib/generators/sage_component/sage_component_generator.rb
@@ -3,7 +3,7 @@ class SageComponentGenerator < Rails::Generators::NamedBase
   def create_sage_component
     style_line = "// Components"
     component_line = "["
-    create_file "app/views/example_components/#{file_name}/_preview.html.erb", "<div class=\"sage-#{file_name}\">\n\n</div>"
+    create_file "app/views/example_components/#{file_name}/_preview.html.erb", "<div class=\"sage-#{file_name}\"></div>"
     create_file "app/assets/stylesheets/sage/patterns/components/_#{file_name}.scss", "/*------------------------------------*/\n/*-- #{file_name} component */\n/*------------------------------------*/\n\n.sage-#{file_name} {\n  \n  /* #{file_name} responsive styles */\n  @media (max-width: sage-breakpoint()) {\n    \n  }\n}"
     gsub_file 'app/assets/stylesheets/sage.css.scss', /(#{Regexp.escape(style_line)})/mi do |match|
       "#{match}\n@import \"sage/patterns/components/#{file_name}\";"
