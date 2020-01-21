6a4cbcf189fd7c327d049c792e6d36fa70fbb8d3
cmcfadzean <court@kajabi.com>
2020-01-21 15:41:53 -0800

    cleaning up generators

--- lib/generators/sage_component/sage_component_generator.rb
+++ lib/generators/sage_component/sage_component_generator.rb
@@ -1,19 +1,30 @@
 class SageComponentGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates', __FILE__)
   def create_sage_component
-    style_path = "app/assets/stylesheets/sage/patterns/components/_#{file_name}.scss"
-    markup_path = "app/views/example_components/#{file_name}/_preview.html.erb"
-    style_line = "// Components"
-    helper_line = "["
 
-    template "markup.html.erb", markup_path
-    template "style.scss", style_path
+    # Style Variables
+    style_file = "app/assets/stylesheets/sage/patterns/components/_#{file_name}.scss"
+    style_include_file = "app/assets/stylesheets/sage.css.scss"
+    style_include_line = "// Components"
+    style_include_content = "#{match}\n@import \"sage/patterns/components/#{file_name}\";"
+    # Create Style File
+    template "style.scss", style_file
+    # Include Style File
+    gsub_file style_include_file, /(#{Regexp.escape(style_include_line)})/mi do |match|
+      style_include_content
+    end
+    
+    # Markup Variables
+    markup_file = "app/views/example_components/#{file_name}/_preview.html.erb"
+    markup_include_file = "app/helpers/sage/components_helper.rb"
+    markup_include_line = "["
+    markup_include_content = "#{match}\n        { title: \"#{file_name}\" },"
+    # Create Markup File
+    template "markup.html.erb", markup_file
+    # Include Markup File
+    gsub_file markup_include_file, /(#{Regexp.escape(markup_include_line)})/mi do |match|
+      markup_include_content
+    end
 
-    gsub_file 'app/assets/stylesheets/sage.css.scss', /(#{Regexp.escape(style_line)})/mi do |match|
-      "#{match}\n@import \"sage/patterns/components/#{file_name}\";"
-    end
-    gsub_file 'app/helpers/sage/components_helper.rb', /(#{Regexp.escape(helper_line)})/mi do |match|
-      "#{match}\n        { title: \"#{file_name}\" },"
-    end
   end
 end
--- lib/generators/sage_element/sage_element_generator.rb
+++ lib/generators/sage_element/sage_element_generator.rb
@@ -1,15 +1,30 @@
 class SageElementGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates', __FILE__)
   def create_sage_element
-    style_line = "// Elements"
-    helper_line = "["
-    create_file "app/views/example_elements/#{file_name}/_preview.html.erb", "<div class=\"sage-#{file_name}\"></div>"
-    create_file "app/assets/stylesheets/sage/patterns/elements/_#{file_name}.scss", "/*------------------------------------*/\n/*-- #{file_name} element */\n/*------------------------------------*/\n\n.sage-#{file_name} {\n  \n  /* #{file_name} responsive styles */\n  @media (max-width: sage-breakpoint()) {\n    \n  }\n}"
-    gsub_file 'app/assets/stylesheets/sage.css.scss', /(#{Regexp.escape(style_line)})/mi do |match|
-      "#{match}\n@import \"sage/patterns/elements/#{file_name}\";"
+
+    # Style Variables
+    style_file = "app/assets/stylesheets/sage/patterns/elements/_#{file_name}.scss"
+    style_include_file = "app/assets/stylesheets/sage.css.scss"
+    style_include_line = "// Elements"
+    style_include_content = "#{match}\n@import \"sage/patterns/elements/#{file_name}\";"
+    # Create Style File
+    template "style.scss", style_file
+    # Include Style File
+    gsub_file style_include_file, /(#{Regexp.escape(style_include_line)})/mi do |match|
+      style_include_content
     end
-    gsub_file 'app/helpers/sage/elements_helper.rb', /(#{Regexp.escape(helper_line)})/mi do |match|
-      "#{match}\n        { title: \"#{file_name}\" },"
+    
+    # Markup Variables
+    markup_file = "app/views/example_elements/#{file_name}/_preview.html.erb"
+    markup_include_file = "app/helpers/sage/elements_helper.rb"
+    markup_include_line = "["
+    markup_include_content = "#{match}\n        { title: \"#{file_name}\" },"
+    # Create Markup File
+    template "markup.html.erb", markup_file
+    # Include Markup File
+    gsub_file markup_include_file, /(#{Regexp.escape(markup_include_line)})/mi do |match|
+      markup_include_content
     end
+
   end
 end
