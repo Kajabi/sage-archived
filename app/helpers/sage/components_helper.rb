31ed3b5e8c5efebc9d0430765e25df06ada7907b
cmcfadzean <court@kajabi.com>
2020-01-21 11:51:51 -0800

    adding updates to links

--- app/helpers/sage/components_helper.rb
+++ app/helpers/sage/components_helper.rb
@@ -2,6 +2,7 @@ module Sage
   module ComponentsHelper
     def sage_components
       [
+        { title: "sam", path: "sam" },
         { title: "Banner", path: "banner" },
         { title: "Panel", path: "panel" },
       ]
--- lib/generators/sage_component/sage_component_generator.rb
+++ lib/generators/sage_component/sage_component_generator.rb
@@ -1,7 +1,15 @@
 class SageComponentGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates', __FILE__)
   def create_sage_component
+    style_line = "// Components"
+    component_line = "["
     create_file "app/views/example_components/#{file_name}/#{file_name}.html.erb", "<div class=\"sage-#{file_name}\">\n\n</div>"
     create_file "app/assets/stylesheets/sage/patterns/components/_#{file_name}.scss", "/*------------------------------------*/\n/*-- #{file_name} component */\n/*------------------------------------*/\n\n.sage-#{file_name} {\n  \n  /* #{file_name} responsive styles */\n  @media (max-width: sage-breakpoint()) {\n    \n  }\n}"
+    gsub_file 'app/assets/stylesheets/sage.css.scss', /(#{Regexp.escape(style_line)})/mi do |match|
+      "#{match}\n@import \"sage/patterns/components/#{file_name}\";"
+    end
+    gsub_file 'app/helpers/sage/components_helper.rb', /(#{Regexp.escape(component_line)})/mi do |match|
+      "#{match}\n        { title: \"#{file_name}\", path: \"#{file_name}\" },"
+    end
   end
 end
