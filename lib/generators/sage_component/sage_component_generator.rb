c3a0c8e31cfebcce06fc91a97048184d0a289cb9
cmcfadzean <court@kajabi.com>
2020-01-21 14:35:01 -0800

    sage module helper

--- lib/generators/sage_component/sage_component_generator.rb
+++ lib/generators/sage_component/sage_component_generator.rb
@@ -2,13 +2,13 @@ class SageComponentGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates', __FILE__)
   def create_sage_component
     style_line = "// Components"
-    component_line = "["
+    helper_line = "["
     create_file "app/views/example_components/#{file_name}/_preview.html.erb", "<div class=\"sage-#{file_name}\"></div>"
     create_file "app/assets/stylesheets/sage/patterns/components/_#{file_name}.scss", "/*------------------------------------*/\n/*-- #{file_name} component */\n/*------------------------------------*/\n\n.sage-#{file_name} {\n  \n  /* #{file_name} responsive styles */\n  @media (max-width: sage-breakpoint()) {\n    \n  }\n}"
     gsub_file 'app/assets/stylesheets/sage.css.scss', /(#{Regexp.escape(style_line)})/mi do |match|
       "#{match}\n@import \"sage/patterns/components/#{file_name}\";"
     end
-    gsub_file 'app/helpers/sage/components_helper.rb', /(#{Regexp.escape(component_line)})/mi do |match|
+    gsub_file 'app/helpers/sage/components_helper.rb', /(#{Regexp.escape(helper_line)})/mi do |match|
       "#{match}\n        { title: \"#{file_name}\", path: \"#{file_name}\" },"
     end
   end
---
+++ lib/generators/sage_module/sage_module_generator.rb
@@ -0,0 +1,15 @@
+class SageModuleGenerator < Rails::Generators::NamedBase
+  source_root File.expand_path('../templates', __FILE__)
+  def create_sage_module
+    style_line = "// Modules"
+    helper_line = "["
+    create_file "app/views/example_modules/#{file_name}/_preview.html.erb", "<div class=\"sage-#{file_name}\"></div>"
+    create_file "app/assets/stylesheets/sage/patterns/modules/_#{file_name}.scss", "/*------------------------------------*/\n/*-- #{file_name} module */\n/*------------------------------------*/\n\n.sage-#{file_name} {\n  \n  /* #{file_name} responsive styles */\n  @media (max-width: sage-breakpoint()) {\n    \n  }\n}"
+    gsub_file 'app/assets/stylesheets/sage.css.scss', /(#{Regexp.escape(style_line)})/mi do |match|
+      "#{match}\n@import \"sage/patterns/modules/#{file_name}\";"
+    end
+    gsub_file 'app/helpers/sage/modules_helper.rb', /(#{Regexp.escape(helper_line)})/mi do |match|
+      "#{match}\n        { title: \"#{file_name}\", path: \"#{file_name}\" },"
+    end
+  end
+end
