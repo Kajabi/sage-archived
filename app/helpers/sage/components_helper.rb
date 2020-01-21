feea2fc4710810891604daf235537f31987bb6f5
cmcfadzean <court@kajabi.com>
2020-01-21 11:52:07 -0800

    removing dummy content

--- app/helpers/sage/components_helper.rb
+++ app/helpers/sage/components_helper.rb
@@ -2,7 +2,6 @@ module Sage
   module ComponentsHelper
     def sage_components
       [
-        { title: "sam", path: "sam" },
         { title: "Banner", path: "banner" },
         { title: "Panel", path: "panel" },
       ]
