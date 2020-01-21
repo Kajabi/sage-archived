dd29e49f7baed7c794388f1c2cc5a78256668c9c
cmcfadzean <court@kajabi.com>
2020-01-21 15:24:06 -0800

    adding templates

--- lib/generators/sage_component/sage_component_generator.rb
+++ lib/generators/sage_component/sage_component_generator.rb
@@ -1,10 +1,14 @@
 class SageComponentGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates', __FILE__)
   def create_sage_component
+    style_path = "app/assets/stylesheets/sage/patterns/components/_#{file_name}.scss"
+    markup_path = "app/views/example_components/#{file_name}/_preview.html.erb"
     style_line = "// Components"
     helper_line = "["
-    create_file "app/views/example_components/#{file_name}/_preview.html.erb", "<div class=\"sage-#{file_name}\"></div>"
-    create_file "app/assets/stylesheets/sage/patterns/components/_#{file_name}.scss", "/*------------------------------------*/\n/*-- #{file_name} component */\n/*------------------------------------*/\n\n.sage-#{file_name} {\n  \n  /* #{file_name} responsive styles */\n  @media (max-width: sage-breakpoint()) {\n    \n  }\n}"
+
+    template "markup.html.erb", markup_path
+    template "style.scss", style_path
+
     gsub_file 'app/assets/stylesheets/sage.css.scss', /(#{Regexp.escape(style_line)})/mi do |match|
       "#{match}\n@import \"sage/patterns/components/#{file_name}\";"
     end
