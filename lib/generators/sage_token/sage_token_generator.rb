2c0d6fe83472cb7fc3a9b2e92c9e7c6d33bc7f5a
cmcfadzean <court@kajabi.com>
2020-01-22 15:34:19 -0800

    updating generator to include style documetnation

--- lib/generators/sage_token/sage_token_generator.rb
+++ lib/generators/sage_token/sage_token_generator.rb
@@ -21,5 +21,13 @@ class SageTokenGenerator < Rails::Generators::NamedBase
       "#{match}\n        {\n          category: \"#{file_name}\",\n          tokens: [\n            { name: \"default\" },\n          ]\n        },"
     end
 
+    # Documentation Style Variables
+    doc_include_file = "app/assets/stylesheets/sage_docs/_token.scss"
+    doc_include_line = "// Generator Tokens"
+    # Include Documentation Style
+    gsub_file doc_include_file, /(#{Regexp.escape(doc_include_line)})/mi do |match|
+      "#{match}\n  &-spacing:after { content: \"#{sage-spacing()}\" }\n  @each $name, $token in $sage-spacings {\n    &-spacing-#{$name}:after { content: \"#{$token}\" }\n  }"
+    end
+
   end
 end
