afdebe0cc5db08fb5540259442e81f02cd56773e
Kevin Chang <kevin@kajabi.com>
2020-02-19 10:27:20 -0800

    enable app JS, use rails loader for ext JS

---
+++ test/dummy/app/assets/javascripts/sage-docs.js
@@ -0,0 +1,12 @@
+/**
+	* sage-docs.js
+	*
+	* Routes commands for sage documentation functionality
+	*
+*/
+
+if (document.querySelector('.sage-content')) {
+	console.info('sage docs active');
+
+
+}
