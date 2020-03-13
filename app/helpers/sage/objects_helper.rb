34f0f64e5ba39d324178bb261ddb5a0b8a227994
Phil Schanely <philschanely@gmail.com>
2020-03-13 10:30:15 -0400

    Add Live Avatar (#89)
    * Add sample avatar images
    
    * Add avatar object and restructure image location
    
    * Improve settings and documentation
    
    * Further modifications to settings
    
    * Add documentation around avatar sizing
    
    * Adjust variable scope practice
    
    * Fix linting on mixins
    
    * Fix variable bugs
    
    * Add rem function
    
    * Use sage-border function

--- app/helpers/sage/objects_helper.rb
+++ app/helpers/sage/objects_helper.rb
@@ -2,10 +2,10 @@ module Sage
   module ObjectsHelper
     ########################################################
     # STATUS KEY
-    # Done : done 
-    # In Progress : doing 
-    # To-do : todo 
-    # Not applicable : no 
+    # Done : done
+    # In Progress : doing
+    # To-do : todo
+    # Not applicable : no
     # Deprecated : stop
     ########################################################
 
@@ -13,7 +13,20 @@ module Sage
     def sage_objects
       [
         # Sage Generated Objects
-        { 
+        {
+          title: "live_avatar",
+          description: "The Live Avatar object shows a user's profile image in a circular frame and allows for a few helpful modifications for different uses.",
+          scss_design:  "doing",
+          scss_dev:     "doing",
+          scss_doc:     "doing",
+          rails_design: "todo",
+          rails_dev:    "todo",
+          rails_doc:    "todo",
+          react_design: "todo",
+          react_dev:    "todo",
+          react_doc:    "todo"
+        },
+        {
           title: "form_section",
           description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
           scss_design:  "todo",
@@ -26,7 +39,7 @@ module Sage
           react_dev:    "todo",
           react_doc:    "todo"
         },
-        { 
+        {
           title: "tabs",
           description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
           scss_design:  "todo",
@@ -39,7 +52,7 @@ module Sage
           react_dev:    "todo",
           react_doc:    "todo"
         },
-        { 
+        {
           title: "banner",
           description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
           scss_design:  "todo",
@@ -52,7 +65,7 @@ module Sage
           react_dev:    "todo",
           react_doc:    "todo"
         },
-        { 
+        {
           title: "panel",
           description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
           scss_design:  "todo",
@@ -65,7 +78,7 @@ module Sage
           react_dev:    "todo",
           react_doc:    "todo"
         },
-        { 
+        {
           title: "assistant",
           description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
           scss_design:  "todo",
@@ -78,7 +91,7 @@ module Sage
           react_dev:    "todo",
           react_doc:    "todo"
         },
-        { 
+        {
           title: "nav",
           description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
           scss_design:  "todo",
@@ -89,9 +102,9 @@ module Sage
           rails_doc:    "todo",
           react_design: "todo",
           react_dev:    "todo",
-          react_doc:    "todo" 
+          react_doc:    "todo"
         },
-        { 
+        {
           title: "sidebar",
           description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
           scss_design:  "todo",
@@ -106,7 +119,7 @@ module Sage
         },
       ]
     end
-    
+
     # Sorts available objects based on alphabet
     def sorted_sage_objects
       sage_objects.sort_by { |h| h[:title] }
