ff9b336d6ea602d1c0b4694c833f151d3f9f2fc8
Phillip Lovelace <phillip@pixelflips.com>
2020-03-17 13:37:16 -0700

    tooltip js adjustments

--- app/assets/javascripts/tooltip.js
+++ app/assets/javascripts/tooltip.js
@@ -3,15 +3,18 @@ Tooltip = function(options) {
   
     document.body.addEventListener("mouseover", function(e) {
       if (!e.target.hasAttribute("data-tooltip")) return;
+
       var tooltip = document.createElement("div");
-      var pos = e.target.getAttribute("data-position").split(" ")[0];
+      var pos = e.target.getAttribute("data-position") || "top";
       tooltip.className = "sage-tooltip";
       tooltip.innerHTML = e.target.getAttribute("data-tooltip");
       tooltip.theme = e.target.getAttribute("data-tooltip-theme");
       tooltip.position = e.target.getAttribute("data-position");
+
       if (tooltip.theme) {
         tooltip.classList.add("sage-tooltip--light");
       }
+
       document.body.appendChild(tooltip);
       positionTooltip(e.target, tooltip, pos);
     });
@@ -25,9 +28,8 @@ Tooltip = function(options) {
     });
   
     function positionTooltip(parent, tooltip, position) {
-      var parentCoords = parent.getBoundingClientRect(),
-        left,
-        top;
+      var parentCoords = parent.getBoundingClientRect(), left, top;
+
       switch (position) {
         case "left":
           top = (parseInt(parentCoords.top) + parseInt(parentCoords.bottom)) / 2 - tooltip.offsetHeight / 2;
@@ -35,23 +37,24 @@ Tooltip = function(options) {
           if (parseInt(parentCoords.left) - tooltip.offsetWidth < 0) {
             left = dist;
           }
-        break;
+          break;
         case "right":
           top = (parseInt(parentCoords.top) + parseInt(parentCoords.bottom)) / 2 - tooltip.offsetHeight / 2;
           left = parentCoords.right + dist;
           if (parseInt(parentCoords.right) + tooltip.offsetWidth > document.documentElement.clientWidth) {
             left = document.documentElement.clientWidth - tooltip.offsetWidth - dist;
           }
-        break;
+          break;
         case "bottom":
             top = parseInt(parentCoords.bottom) + dist;
             left = parseInt(parentCoords.left) + (parent.offsetWidth - tooltip.offsetWidth) / 2;
-        break;
+          break;
         default:
         case "top":
           top = parseInt(parentCoords.top) - tooltip.offsetHeight - dist;
           left = parseInt(parentCoords.left) + (parent.offsetWidth - tooltip.offsetWidth) / 2;
       }
+
       tooltip.style.left = left + "px";
       tooltip.style.top = top + pageYOffset + "px";
       tooltip.classList.add("sage-tooltip-" + position);
@@ -59,5 +62,5 @@ Tooltip = function(options) {
   };
 
   var tooltip = new Tooltip({
-    distance: 8,    // Specifies the distance in pixels from trigger to tooltip.
+    distance: 8,  // distance in pixels from trigger to tooltip.
   });
