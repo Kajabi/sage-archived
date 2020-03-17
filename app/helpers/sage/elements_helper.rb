736fbf0c981665d3cc78ac179ee59c5e41f3cc04
Phillip Lovelace <phillip@pixelflips.com>
2020-03-17 11:27:50 -0700

    initial commits for tooltips

--- app/helpers/sage/elements_helper.rb
+++ app/helpers/sage/elements_helper.rb
@@ -14,6 +14,19 @@ module Sage
       [
         # Sage Generated Elements
         {
+          title: "tooltip",
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", 
+          scss_design:  "todo",
+          scss_dev:     "todo",
+          scss_doc:     "todo",
+          rails_design: "todo",
+          rails_dev:    "todo",
+          rails_doc:    "todo",
+          react_design: "todo",
+          react_dev:    "todo",
+          react_doc:    "todo"
+        },
+        {
           title: "form_input",
           description: "Basic text input form fields with 'floating' labels",
           scss_design:  "done",
