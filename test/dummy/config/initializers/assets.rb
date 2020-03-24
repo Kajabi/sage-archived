b2624cb6974f7d64d70575a3571cf6602ced59b1
cmcfadzean <court@kajabi.com>
2020-03-24 13:42:51 -0700

    moving precompile to sage gem

--- test/dummy/config/initializers/assets.rb
+++ test/dummy/config/initializers/assets.rb
@@ -9,14 +9,3 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-
-Rails.application.config.assets.precompile += %w( sage_docs.js )
-
-Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
-Rails.application.config.assets.precompile += %w( sage.css )
-Rails.application.config.assets.precompile += %w( sage_docs.css )
-
-Rails.application.config.assets.precompile += %w( sage/sage.svg )
-Rails.application.config.assets.precompile += %w( sage/sage_icon.svg )
-Rails.application.config.assets.precompile += %w( sage/sage_illustration.png )
-Rails.application.config.assets.precompile += %w( sage/sage_structure.png )
