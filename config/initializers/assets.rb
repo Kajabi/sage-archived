7fbc016a49b790b09a7e6c971e3d9246441fe61d
cmcfadzean <court@kajabi.com>
2020-03-24 13:52:38 -0700

    adding assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,7 +8,7 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
+Rails.application.config.assets.precompile << /\.(?:svg|png|jpg|eot|woff|ttf)$/
 
 Rails.application.config.assets.precompile += %w( sage.css )
 Rails.application.config.assets.precompile += %w( sage_docs.css )
