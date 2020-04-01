a09ba1318ed6b957552bc47ebbf3d9383b79ba2f
Andrew McIntee <contact@andrew.mn>
2020-04-01 13:44:29 -0600

    Generate select element

--- app/helpers/sage/elements_helper.rb
+++ app/helpers/sage/elements_helper.rb
@@ -19,6 +19,13 @@ module Sage
           scss_design:  "done",
           scss_dev:     "done",
           scss_doc:     "done",
+        },
+        {
+          title: "select",
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+          scss_design:  "done",
+          scss_dev:     "done",
+          scss_doc:     "todo",
           rails_design: "todo",
           rails_dev:    "todo",
           rails_doc:    "todo",
