f02c9f70c82930d4f85781d3ef50cbe014f680ed
Phil Schanely <philschanely@gmail.com>
2020-04-06 15:19:34 -0400

    Final tweaks

--- app/helpers/sage/objects_helper.rb
+++ app/helpers/sage/objects_helper.rb
@@ -15,13 +15,13 @@ module Sage
         # Sage Generated Objects
         {
           title: "live_stream_video_grid",
-          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", 
-          scss_design:  "todo",
-          scss_dev:     "todo",
-          scss_doc:     "todo",
-          rails_design: "todo",
-          rails_dev:    "todo",
-          rails_doc:    "todo",
+          description: "Adds the video grid setup for live stream. Toggle the `--fullscreen-demo` modifier on the wrapper to see at full screen.", 
+          scss_design:  "done",
+          scss_dev:     "done",
+          scss_doc:     "done",
+          rails_design: "no",
+          rails_dev:    "no",
+          rails_doc:    "no",
           react_design: "todo",
           react_dev:    "todo",
           react_doc:    "todo"
