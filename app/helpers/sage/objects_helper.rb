90f72b310ab0b3f09e504d2a5b17f951b5096a68
cmcfadzean <court@kajabi.com>
2020-04-28 08:23:58 -0700

    updating the status page

--- app/helpers/sage/objects_helper.rb
+++ app/helpers/sage/objects_helper.rb
@@ -16,12 +16,12 @@ module Sage
         {
           title: "billboard",
           description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", 
-          scss_design:  "todo",
-          scss_dev:     "todo",
-          scss_doc:     "todo",
-          rails_design: "todo",
-          rails_dev:    "todo",
-          rails_doc:    "todo",
+          scss_design:  "done",
+          scss_dev:     "done",
+          scss_doc:     "done",
+          rails_design: "doing",
+          rails_dev:    "doing",
+          rails_doc:    "doing",
           react_design: "todo",
           react_dev:    "todo",
           react_doc:    "todo"
