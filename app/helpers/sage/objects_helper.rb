7973c4949e0a497f6969d170b4adef7c50bcd19a
cmcfadzean <court@kajabi.com>
2020-04-27 20:31:38 -0700

    adding billboard object

--- app/helpers/sage/objects_helper.rb
+++ app/helpers/sage/objects_helper.rb
@@ -14,6 +14,19 @@ module Sage
       [
         # Sage Generated Objects
         {
+          title: "billboard",
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", 
+          scss_design:  "todo",
+          scss_dev:     "todo",
+          scss_doc:     "todo",
+          rails_design: "todo",
+          rails_dev:    "todo",
+          rails_doc:    "todo",
+          react_design: "todo",
+          react_dev:    "todo",
+          react_doc:    "todo"
+        },
+        {
           title: "pagination",
           description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
           scss_design:  "done",
