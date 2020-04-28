099b4d5ac8f43b21c96e70d20248a66ac22d9739
Andrew McIntee <contact@andrew.mn>
2020-04-28 09:47:03 -0500

    Sage-select variable cleanup

--- app/helpers/sage/elements_helper.rb
+++ app/helpers/sage/elements_helper.rb
@@ -15,9 +15,9 @@ module Sage
         # Sage Generated Elements
         {
           title: "form_select",
-          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", 
-          scss_design:  "todo",
-          scss_dev:     "todo",
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+          scss_design:  "done",
+          scss_dev:     "done",
           scss_doc:     "todo",
           rails_design: "todo",
           rails_dev:    "todo",
