5b971588fe07a1f92801d4b8f278b523e363af19
Andrew McIntee <contact@andrew.mn>
2020-04-29 16:46:04 -0500

    Compile packs to test/dummy in development

--- lib/sage/engine.rb
+++ lib/sage/engine.rb
@@ -11,23 +11,23 @@ require "inline_svg" # DELETE AJM
 
 module Sage
   class Engine < ::Rails::Engine
-    # binding.pry
     isolate_namespace Sage
 
     initializer "webpacker.proxy" do |app|
-      # binding.pry
-        insert_middleware = begin
-                            Sage.webpacker.config.dev_server.present?
-                          rescue
-                            nil
-                          end
-        next unless insert_middleware
-
-        app.middleware.insert_before(
-          0, Webpacker::DevServerProxy, # "Webpacker::DevServerProxy" if Rails version < 5
-          ssl_verify_none: true,
-          webpacker: Sage.webpacker
-        )
+      insert_middleware = begin
+        Sage.webpacker.config.dev_server.present?
+      rescue
+        nil
       end
+
+      next unless insert_middleware
+
+      app.middleware.insert_before(
+        0,
+        "Webpacker::DevServerProxy", # "Webpacker::DevServerProxy" as a string if Rails version < 5
+        ssl_verify_none: true,
+        webpacker: Sage.webpacker
+      )
+    end
   end
 end
