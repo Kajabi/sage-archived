b943f2606d3ddd52a3fc56da0101b6d57d916f3f
Andrew McIntee <contact@andrew.mn>
2020-04-30 12:18:40 -0500

    Migrate middleware that serves webpacker files outside of dummy and into engine so that it can be shared with kajabi-products

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -19,13 +19,6 @@ module Dummy
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    config.app_middleware.use(
-      Rack::Static,
-      urls: ['/sage/packs'],
-      root: Sage::Engine.root.join("public")
-    )
-    # binding.pry
-
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
