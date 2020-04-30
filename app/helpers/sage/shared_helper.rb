812d03a344a5ee6e47cee68791baf67cff363fd3
Andrew McIntee <contact@andrew.mn>
2020-04-30 12:04:47 -0500

    Webpack dev server hot reloading

---
+++ app/helpers/sage/shared_helper.rb
@@ -0,0 +1,20 @@
+module Sage
+  module SharedHelper
+
+    # def sage_stylesheet_tag
+    #   # binding.pry
+    #   if local_sage_gem
+    #     return stylesheet_pack_tag("application")
+    #   else
+    #     return stylesheet_link_tag("sage/sage_system", media: "all")
+    #   end
+    # end
+
+    # private
+
+    # def local_sage_gem
+    #   ENV["LOCAL_SAGE_GEM"] == "true"
+    # end
+
+  end
+end
