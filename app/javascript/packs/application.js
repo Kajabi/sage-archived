5342cc95fbd8630c3a4b39bba8fa2ceb4347898d
Andrew McIntee <contact@andrew.mn>
2020-04-30 11:36:30 -0500

    Server webpacker packs from outside of dummy app

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,7 @@ import 'core-js/stable'
 import 'regenerator-runtime/runtime'
 import 'stylesheets/application.css' // REQUIRES POST CSS CONFIG
 
-console.warn("WEBPACKEa")
+console.warn("WEBPACKER")
 
 // If kajabi has env local sage gem use generated pack tags locally and use local gem else use /dist files that are fingerprinted with gem version
 
--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -19,8 +19,14 @@ module Dummy
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.app_middleware.use(
+      Rack::Static,
+      urls: ['/sage/packs'],
+      root: Sage::Engine.root.join("public")
+    )
+    # binding.pry
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
-
