d48dddb470cf84e71650ede4852ac58820656a53
Andrew McIntee <contact@andrew.mn>
2020-04-30 13:06:02 -0500

    Successfully loading webpack pack tags into kajabi products

--- app/helpers/sage/shared_helper.rb
+++ app/helpers/sage/shared_helper.rb
@@ -1,20 +1,20 @@
 module Sage
   module SharedHelper
+    include Sage::WebpackerHelper
 
-    # def sage_stylesheet_tag
-    #   # binding.pry
-    #   if local_sage_gem
-    #     return stylesheet_pack_tag("application")
-    #   else
-    #     return stylesheet_link_tag("sage/sage_system", media: "all")
-    #   end
-    # end
+    def sage_system_stylesheet
+      stylesheet_pack_tag("application")
+    end
 
-    # private
+    def sage_system_javascript
+      javascript_pack_tag("application")
+    end
 
-    # def local_sage_gem
-    #   ENV["LOCAL_SAGE_GEM"] == "true"
-    # end
+    private
+
+    def local_sage_gem
+      ENV["LOCAL_SAGE_GEM"] == "true"
+    end
 
   end
 end
