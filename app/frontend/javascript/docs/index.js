3ffe4e4081f062480cf2a08d6390ed9520ea64f2
Andrew McIntee <contact@andrew.mn>
2020-05-01 14:04:16 -0500

    SYSTEM WEBPACK MIGRATE: Copy asset-pipeline assets to webpacker packs

---
+++ app/frontend/javascript/docs/index.js
@@ -0,0 +1,5 @@
+require("../define")
+require("./banner")
+require("./live-option-menu")
+
+console.warn("JS DOCS!")
--- app/frontend/packs/sageDocs.js
+++ app/frontend/packs/sageDocs.js
@@ -1,10 +1,13 @@
-import 'core-js/stable'
-import 'regenerator-runtime/runtime'
+// --- Webpacker Dependancies ---
+import "core-js/stable"
+import "regenerator-runtime/runtime"
 
-import 'stylesheets/application.css'
-import 'javascript/application.js'
+// --- Sage Docs --------------
+import "stylesheets/docs/index.scss"
+import "javascript/docs/index"
 
 console.warn("WEBPACKER")
+
 
 // If kajabi has env local sage gem use generated pack tags locally and use local gem else use /dist files that are fingerprinted with gem version
 
