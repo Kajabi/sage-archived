5888f140e3e9aae70f75a4de9baf175cc4abc969
Andrew McIntee <contact@andrew.mn>
2020-05-02 12:40:58 -0500

    Remove AssetPipeline Migration: moving to seperate PR

--- app/frontend/packs/sageDocs.js
+++ app/frontend/packs/sageDocs.js
@@ -1,4 +1,3 @@
-
 import "./shared"
 
 // --- Sage Images --------------
@@ -9,10 +8,4 @@ import "stylesheets/docs/index.scss"
 import "javascript/docs/index"
 
 
-console.warn("WEBPACKER")
-
-
-// If kajabi has env local sage gem use generated pack tags locally and use local gem else use /dist files that are fingerprinted with gem version
-
-// Serve partial with fingerprinted (with version) to kajabi-products
-// If env is development inside partial point to webpacker packs
+console.warn("WEBPACKER DOCS HAS LOADED")
