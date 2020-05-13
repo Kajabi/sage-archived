8be4ece32732c80258660b30b01d3c96ca061ee1
Andrew McIntee <contact@andrew.mn>
2020-05-13 08:54:22 -0500

    testing some other configurations to get webpacker sage over to kajabi-products

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -21,6 +21,10 @@ module Dummy
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.middleware.use(
+      Rack::Static,
+      urls: ["/sage/packs"], root: Sage::Engine.root.join("tmp")
+    )
   end
 end
-
