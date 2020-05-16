6d8f23c5384d8b621413184477b15901f758856d
Kevin Chang <kevin@kajabi.com>
2020-05-15 17:29:31 -0700

    styles update, example JS for testing

--- app/assets/javascripts/sage/system/meter.js
+++ app/assets/javascripts/sage/system/meter.js
@@ -1,47 +1,57 @@
 Sage.meter = (function() {
 
   // ==================================================
-  // Variables
-  // ==================================================
-  var meter = Sage.util.nodelistToArray(document.querySelectorAll('.sage-meter'));
-
-  // ==================================================
   // Functions
   // ==================================================
 
-  function updateMeterValues(ele) {
-    var meter = document.querySelector('[js-meter-type="password"]');
+  // builds an object from meter data
+  function getMeterValues(ele) {
+    var meter = document.querySelector(ele);
 
-    console.info('current', getCurrentValue(meter));
-    console.info('optimum', attributeValue(meter, "optimum"));
-    console.info('min', attributeValue(meter, "min"));
-    console.info('max', attributeValue(meter, "max"));
+    return {
+      current: meter.value,
+      optimum: attributeValue(meter, "optimum"),
+      min: attributeValue(meter, "min"),
+      max: attributeValue(meter, "max")
+    };
   }
 
+
+  // appends classes to meter for styling of current state
+  function updateMeter(ele) {
+    var meter = document.querySelector(ele),
+        values = getMeterValues(ele);
+
+    // reset any meter classes
+    meter.className = "sage-meter__bar";
+
+    // assign new class based on current value
+    if (values.current === values.max) {
+      meter.classList.add('sage-meter__bar--max');
+    } else if ((values.optimum !== 0) && values.current >= values.optimum) {
+      meter.classList.add('sage-meter__bar--optimum');
+    } else if ((values.current <= values.max) && (values.current > values.min))  {
+      meter.classList.add('sage-meter__bar--low');
+    }
+  }
+
+
+  // retrieve attribute value or default to 0
   function attributeValue(ele, attributeName) {
-    return ele.hasAttribute(attributeName) ? ele.getAttribute(attributeName) : 0;
-  }
-
-  function getCurrentValue(ele) {
-    return ele.value;
-  }
-
-
-  function bindEvents() {
-
+    return ele.hasAttribute(attributeName) ? parseInt(ele.getAttribute(attributeName)) : 0;
   }
 
 
   function init() {
     if (document.querySelector('[js-meter-type="password"]') !== null) {
-      updateMeterValues();
-      bindEvents();
+      updateMeter('[js-meter-type="password"]');
     }
   }
 
+
   return {
     init: init,
-    updateMeter: updateMeterValues
+    updateMeter: updateMeter
   };
 
 })();
