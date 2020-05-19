00e31de7b45edcbd7eb6db238964df3fb1dba769
Kevin Chang <kevin@kajabi.com>
2020-05-18 21:33:38 -0700

    working criteria logic

--- app/assets/javascripts/sage/system/inputhelper.js
+++ app/assets/javascripts/sage/system/inputhelper.js
@@ -6,6 +6,10 @@ Sage.inputhelper = (function() {
   var spcChar = /(?=.*[~`!@#$%^&*|(){}/=+-])/g;
   var numChar = /(?=[0-9])/g;
 
+  var visibleHintClass = "sage-input-helper--visible";
+  var passingClass = "sage-hint__list-item--success";
+  var inputErrorClass = "sage-input--error";
+
 
   // ==================================================
   // Functions
@@ -15,42 +19,67 @@ Sage.inputhelper = (function() {
   function updatePWStrength(inputValue, meter) {
     if (typeof zxcvbn !== "undefined") {
       meter.value = zxcvbn(inputValue).score;
-      console.info(meter.value);
+      // console.info(meter.value);
     } else {
       meter.closest(".sage-meter").style.opacity = "0";
     }
   }
 
   // check for special characters
-  function pwSpecial(str) {
+  function pwSpecial(str, num) {
+    if (num == 0) return true;
     return spcChar.test(str);
   }
 
   // check for numbers
-  function pwNumber(str) {
+  function pwNumber(str, num) {
+    if (num == 0) return true;
     return numChar.test(str);
   }
 
-  // check for length
-  function pwLength(str) {
-    return str.length >= 8;
+  // check for password length
+  function pwLength(str, num) {
+    if (num === 0) return true;
+    return str.length >= num;
+  }
+
+  function checkRequirements(ele, reqs) {
+    var val = ele.value;
+    var metChar = false;
+    var metSym = false;
+    var metNum = false;
+
+    reqs.forEach(function(item) {
+      if (item.type === "characters") {
+        metChar = updateCriteria(pwLength(val, item.minValue), item.id);
+      } else if (item.type === "symbols") {
+        metSym = updateCriteria(pwSpecial(val, item.minValue), item.id);
+      } else if (item.type === "numbers") {
+        metNum = updateCriteria(pwNumber(val, item.minValue), item.id);
+      }
+    });
+
+    if (metChar && metSym && metNum) {
+      ele.parentElement.classList.remove(inputErrorClass);
+    }
   }
 
   // toggles item criteria
-  function testCriteria(obj) {
-    console.info(obj);
-    // var passed = document.querySelector(ele.getAttribute(attribute));
+  function updateCriteria(bool, itemID) {
+    var criteria = document.getElementById(itemID);
 
-    // if (bool === true ) {
-    //   passed.classList.add("passed");
-    // } else {
-    //   passed.classList.remove("passed");
-    // }
+    if (bool === true ) {
+      criteria.classList.add(passingClass);
+      return true;
+    } else {
+      criteria.classList.remove(passingClass);
+      return false;
+    }
   }
 
 
   // toggles visibility of helper window
-  function toggleHint(ele, className) {
+  function toggleHintIE(ele, className) {
     if (ele.classList.contains(className)) {
       ele.classList.remove(className);
     } else {
@@ -62,57 +91,53 @@ Sage.inputhelper = (function() {
   // trigger classes for active state in IE
   function focusBlurIE(field) {
     field.addEventListener("focus", function(e) {
-      toggleHint(helper, "sage-input-helper--visible");
+      toggleHintIE(helper, visibleHintClass);
     });
     field.addEventListener("blur", function(e) {
-      toggleHint(helper, "sage-input-helper--visible");
+      toggleHintIE(helper, visibleHintClass);
     });
   }
 
 
-  function helperEvents(helper) {
-    var fieldID = helper.getAttribute("data-js-helper-target");
-    var field = document.getElementById(fieldID);
+  // bind events related to each input helper for passwords
+  function bindPWEvents(helper) {
+    var fieldID = helper.getAttribute("data-js-helper-target"),
+        field = document.getElementById(fieldID);
+
+    var helperList = Sage.util.nodelistToArray(helper.querySelectorAll(".sage-hint__list-item")),
+        helperReqItems = helperList.map(function(ele) {
+          return {
+            id: ele.id,
+            type: ele.getAttribute("data-js-hint-type") || null,
+            minValue: ele.getAttribute("data-js-hint-min") || 0
+          }
+        });
+
     var meter = helper.querySelector(".sage-meter__bar");
 
-    var helperList = Sage.util.nodelistToArray(helper.querySelectorAll(".sage-hint__list-item"));
-    var helperReq = helperList.map(function(ele) {
-      return {
-        type: ele.getAttribute("data-js-hint-type") || null,
-        minValue: ele.getAttribute("data-js-hint-min") || 0
-      }
-    });
-
-    console.info('helperReq', helperReq);
     if (Sage.util.isIE()) {
       focusBlurIE(field);
     }
 
     field.addEventListener("input", function(e) {
-      var targetField = e.target;
+      var targetField = e.target,
+          fieldParent = e.target.parentElement;
 
       // add error state
-      targetField.classList.add("sage-input--error");
+      fieldParent.classList.add(inputErrorClass);
 
-      // targetObj.type === "characters" ? testCriteria(pwLength(targetObj)) : null;
-      // targetObj.type === "symbols" ? testCriteria(pwSpecial(targetObj)) : null;
-      // targetObj.type === "numbers" ? testCriteria(pwNumber(targetObj)) : null;
-
-      // if (pwLength(targetObj.text) && pwSpecial(targetObj.text) && pwNumber(targetObj.text)) {
-      //   targetObj.classList.remove("sage-input--error");
-      // }
-
+      checkRequirements(targetField, helperReqItems);
       updatePWStrength(targetField.value, meter);
     });
   }
 
 
   function init() {
-    if (document.querySelector("[data-js-helper-target]").length !== null) {
+    if (document.querySelector("[data-js-helper-target]") !== null) {
       var helperTargets = Sage.util.nodelistToArray(document.querySelectorAll("[data-js-helper-target]"));
 
       helperTargets.forEach(function(helper) {
-        helperEvents(helper);
+        bindPWEvents(helper);
       });
     }
   }
