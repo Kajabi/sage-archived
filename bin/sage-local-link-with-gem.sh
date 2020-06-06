a84f60ea5ee07b473b6afc59be4b75a60199c689
Andrew McIntee <contact@andrew.mn>
2020-06-05 19:44:18 -0500

    Update sage-local-link script to only setup yarn package

--- bin/sage-local-link-with-gem.sh
+++ bin/sage-local-link-with-gem.sh
@@ -5,38 +5,56 @@ function echo_custom() {
   printf "\n\n\033[0;34m${1} \033[0m${2}\n\033[0;34m----------------------------------------------------------------------------------------------\033[0m\n"
 }
 
-function yarn_install() {
+function bundle_and_yarn() {
+  echo_custom "GEM:" "bundle install"
+  bundle install --quiet
+
   echo_custom "FRONTEND PACKAGE:" "yarn install --force"
   yarn install --silent --force
 }
 
-function show_status_of_package() {
+function show_status_of_gem_and_package() {
+  echo_custom "GEM:" "Current Bundle Config Entries 💎"
+  bundle config
+
   echo_custom "FRONTEND PACKAGE:" "Current Package Symlinks ⚒️"
   ( ls -l node_modules ; ls -l node_modules/@* ) | grep ^l # <-- This looks up the symlinked node_modules
 }
 
 if [ "$1" = "true" ] || [ "$1" = "false" ]; then
 
-  # UNINSTALL Local Bindings
+  # Uninstall Local Bindings
   if [ "$1" = "false" ]; then
+    echo_custom "GEM:" "Removing local Sage gem"
+
+    bundle config --delete disable_local_branch_check
+    bundle config --delete local.Sage
+
     echo_custom "FRONTEND PACKAGE:" "Removing local Sage frontend package"
+
     yarn unlink sage
     (cd $sage_repo_path; yarn unlink)
 
-    yarn_install
+    bundle_and_yarn
     echo_custom "Now Using..." "PRODUCTION SAGE ✅"
 
-  # INSTALL Local Bindings
+  # Install Local Bindings
   elif [ "$1" = "true" ]; then
+    echo_custom "GEM:" "Use local Sage gem located at: $sage_repo_path"
+
+    bundle config --local disable_local_branch_check true
+    bundle config --local local.Sage $sage_repo_path
+
     echo_custom "FRONTEND PACKAGE:" "Use the local Sage frontend package located at: $sage_repo_path"
+
     (cd $sage_repo_path; yarn link)
     yarn link sage
 
-    yarn_install
+    bundle_and_yarn
     echo_custom "Now Using..." "LOCAL SAGE ✅"
   fi;
 
-  show_status_of_package
+  show_status_of_gem_and_package
 
 else
   echo_custom "Run inside the of root the Kajabi-Products repo:"
