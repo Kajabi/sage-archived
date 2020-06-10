d7a6008de72aaad60805c4dda463f0d8f36ea36a
Andrew McIntee <contact@andrew.mn>
2020-06-10 16:02:36 -0500

    Sage webpack migration, migrate over the latest design system components from the rails engine implementation of Sage

--- app/helpers/elements_helper.rb
+++ app/helpers/elements_helper.rb
@@ -13,6 +13,19 @@ module ElementsHelper
     [
       # Sage Generated Elements
       {
+        title: "meter",
+        description: "A horizontal display indicating the measurement of a known (finite) quantity",
+        scss_design:  "done",
+        scss_dev:     "done",
+        scss_doc:     "done",
+        rails_design: "doing",
+        rails_dev:    "doing",
+        rails_doc:    "doing",
+        react_design: "todo",
+        react_dev:    "todo",
+        react_doc:    "todo"
+      },
+      {
         title: "form_select",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
         scss_design:  "done",
@@ -157,19 +170,6 @@ module ElementsHelper
       },
       {
         title: "link_button",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        scss_design:  "todo",
-        scss_dev:     "todo",
-        scss_doc:     "todo",
-        rails_design: "todo",
-        rails_dev:    "todo",
-        rails_doc:    "todo",
-        react_design: "todo",
-        react_dev:    "todo",
-        react_doc:    "todo"
-      },
-      {
-        title: "danger_button",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
         scss_design:  "todo",
         scss_dev:     "todo",
