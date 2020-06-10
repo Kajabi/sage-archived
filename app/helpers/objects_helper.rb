541aa19061502a9d3a77151507c3ab8ce54c1316
Andrew McIntee <contact@andrew.mn>
2020-06-10 16:02:36 -0500

    Sage webpack migration, migrate over the latest design system components from the rails engine implementation of Sage

--- app/helpers/objects_helper.rb
+++ app/helpers/objects_helper.rb
@@ -13,6 +13,32 @@ module ObjectsHelper
     [
       # Sage Generated Objects
       {
+        title: "input_helper",
+        description: "Provides an extended tooltip/popup for text inputs",
+        scss_design:  "doing",
+        scss_dev:     "doing",
+        scss_doc:     "doing",
+        rails_design: "todo",
+        rails_dev:    "todo",
+        rails_doc:    "todo",
+        react_design: "todo",
+        react_dev:    "todo",
+        react_doc:    "todo"
+      },
+      {
+        title: "input_group",
+        description: "Allows inline grouping of text inputs with buttons",
+        scss_design:  "done",
+        scss_dev:     "done",
+        scss_doc:     "done",
+        rails_design: "doing",
+        rails_dev:    "doing",
+        rails_doc:    "doing",
+        react_design: "todo",
+        react_dev:    "todo",
+        react_doc:    "todo"
+      },
+      {
         title: "billboard",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
         scss_design:  "done",
