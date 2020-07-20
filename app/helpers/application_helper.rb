2aab42c44ffd24506a2acf2675bc10df3ff12896
Andrew McIntee <contact@andrew.mn>
2020-07-20 13:05:49 -0400

    Sage Docs: Rails Routing & Navigation Bars (#338)
    - Update routing to use paths instead of query string params to look up elements/objects/breakouts
    - Refactor how we're checking if a page or child page is 'active'
      - Clean up some conventions (like `current?` helper) and take a more "Rails" approach with built in helpers
      - Fixes the collapsing sidebar bug
    - Docs html tidying up

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,15 @@
 module ApplicationHelper
 
-  def current?(key, path)
-    "#{key}" if current_page? path
+  def current_page_styles?
+    params[:title] && current_page?(pages_style_path(params[:title]))
+  end
+
+  def current_page_elements?
+    current_page?(pages_elements_path) || (params[:title] && current_page?(pages_element_path(params[:title])))
+  end
+
+  def current_page_objects?
+    current_page?(pages_objects_path) || (params[:title] && current_page?(pages_object_path(params[:title])))
   end
 
 end
