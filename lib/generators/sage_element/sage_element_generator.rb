35c6597da0b5d030d253ff6fd7f945272569f23c
Andrew McIntee <contact@andrew.mn>
2020-08-04 13:28:09 -0400

    Migrate Sage Generators (#362)
    Migrated Sage generators to standard Rails generator location (out from within `sage-engine`) and updated file paths to point to our new file locations since our reorganization last month. Aside from moving and adjusting a few templates I haven't touched any functionality for the generators. We can come back through and polish now that they work again 😎
    
    This PR also adjusts some `yarn version` preflight checks to restrict version bumping from happening anywhere other than the `master` branch per our new release strategy.

--- lib/generators/sage_element/sage_element_generator.rb
+++ lib/generators/sage_element/sage_element_generator.rb
@@ -3,25 +3,25 @@ class SageElementGenerator < Rails::Generators::NamedBase
   def create_sage_element
 
     # Style Variables
-    style_file = "app/assets/stylesheets/sage/system/patterns/elements/_#{file_name}.scss"
-    style_include_file = "app/assets/stylesheets/sage/sage_system.css.scss"
+    style_file = "lib/sage-frontend/stylesheets/system/patterns/elements_#{file_name}.scss"
+    style_include_file = "lib/sage-frontend/stylesheets/system/index.scss"
     style_include_line = "// Elements"
     # Create Style File
     template "style.scss", style_file
     # Include Style File
     gsub_file style_include_file, /(#{Regexp.escape(style_include_line)})/mi do |match|
-      "#{match}\n@import \"system/patterns/elements/#{file_name}\";"
+      "#{match}\n@import \"patterns/elements/#{file_name}\";"
     end
 
     # Markup Variables
     markup_file = "app/views/examples/elements/#{file_name}/_preview.html.erb"
-    markup_include_file = "app/helpers/sage/elements_helper.rb"
+    markup_include_file = "app/helpers/elements_helper.rb"
     markup_include_line = "# Sage Generated Elements"
     # Create Markup File
     template "markup.html.erb", markup_file
     # Include Markup File
     gsub_file markup_include_file, /(#{Regexp.escape(markup_include_line)})/mi do |match|
-      "#{match}\n        {\n          title: \"#{file_name}\",\n          description: \"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\", \n          scss_design:  \"todo\",\n          scss_dev:     \"todo\",\n          scss_doc:     \"todo\",\n          rails_design: \"todo\",\n          rails_dev:    \"todo\",\n          rails_doc:    \"todo\",\n          react_design: \"todo\",\n          react_dev:    \"todo\",\n          react_doc:    \"todo\"\n        },"
+      "#{match}\n        {\n          title: \"#{file_name}\",\n          description: \"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\", \n          scss: \"todo\",\n          rails: \"todo\",\n          react: \"todo\",\n          a11y: \"todo\",\n        },"
     end
 
     # Props Variables
--- lib/generators/sage_page/sage_page_generator.rb
+++ lib/generators/sage_page/sage_page_generator.rb
@@ -1,10 +1,10 @@
 class SagePageGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates', __FILE__)
   def create_sage_page
-    
+
     # Page Variables
-    page_file = "app/views/sage/pages/#{file_name}.html.erb"
-    page_include_file_controller = "app/controllers/sage/pages_controller.rb"
+    page_file = "app/views/pages/#{file_name}.html.erb"
+    page_include_file_controller = "app/controllers/pages_controller.rb"
     page_include_line_controller = "# Generator Pages"
     page_include_file_routes = "config/routes.rb"
     page_include_line_routes = "do"
