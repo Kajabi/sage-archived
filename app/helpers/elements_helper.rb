6933a62c3eeb40ffd78b93cfefdd19b3a9e05968
Phil Schanely <philschanely@gmail.com>
2020-08-18 14:41:34 -0400

    Sage Lists (#384)
    * Add sage tiles
    
    * Adjust inputs for better tiling
    
    * Lint
    
    * Repalce tiles with simpler list element
    
    * Improve no-wrap example
    
    * Improve documentation
    
    * Fix spacing bug and apply to all serial form fields
    
    * Patch radio size in choice elements

--- app/helpers/elements_helper.rb
+++ app/helpers/elements_helper.rb
@@ -13,6 +13,14 @@ module ElementsHelper
     [
       # Sage Generated Elements
       {
+        title: "lists",
+        description: "Sage lists provide a few styling alternatives for unstyled and inline formatting.", 
+        scss: "todo",
+        rails: "todo",
+        react: "todo",
+        a11y: "todo",
+      },
+      {
         title: "icon_card",
         description: "A simple component that allows an icon to be rendered on a field with a unified color scheme applied.", 
         scss: "done",
