70498b7a19327fda315cf1e6e0f936008ec47146
Andrew McIntee <contact@andrew.mn>
2020-09-03 15:12:43 -0400

    testing

--- lib/sage_rails/app/sage_components/sage_banner.rb
+++ lib/sage_rails/app/sage_components/sage_banner.rb
@@ -1,5 +1,7 @@
 class SageBanner < SageComponent
   attr_accessor :type
   attr_accessor :text
-  attr_accessor :copy
+  attr_accessor :link
+  attr_accessor :icon
+  attr_accessor :context_ladera_top
 end
