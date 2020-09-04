8fb70969e9aaf82de081d93644f1705368754e3f
Andrew McIntee <contact@andrew.mn>
2020-09-04 01:20:03 -0400

    Update local tooling util

--- bin/sage-local-link-frontend-only.sh
+++ bin/sage-local-link-frontend-only.sh
@@ -14,18 +14,12 @@ function echo_custom() {
   printf "\n\n\033[0;34m${1} \033[0m${2}\n\033[0;34m------------------------------------------------\033[0m\n"
 }
 
-function bundle_and_yarn() {
-  echo_custom "GEM:" "bundle install"
-  bundle install --quiet
-
+function yarn_install() {
   echo_custom "FRONTEND PACKAGE:" "yarn install --force"
   yarn install --silent --force
 }
 
-function show_status_of_gem_and_package() {
-  echo_custom "GEM:" "Current Bundle Config Entries 💎"
-  bundle config
-
+function show_status_of_package() {
   echo_custom "FRONTEND PACKAGE:" "Current Package Symlinks ⚒️"
   ( ls -l node_modules ; ls -l node_modules/@* ) | grep ^l # <-- This looks up the symlinked node_modules
 }
@@ -34,35 +28,27 @@ if [ "$1" = "true" ] || [ "$1" = "false" ]; then
 
   # UNINSTALL Local Bindings
   if [ "$1" = "false" ]; then
-    echo_custom "GEM:" "Removing local Sage gem"
-    bundle config --delete disable_local_branch_check
-    bundle config --delete local.Sage
-
     echo_custom "FRONTEND PACKAGE:" "Removing local Sage frontend package"
     yarn unlink sage
     (cd $sage_repo_path; yarn unlink)
 
-    bundle_and_yarn
+    yarn_install
     echo_custom "Now Using..." "PRODUCTION SAGE ✅"
 
   # INSTALL Local Bindings
   elif [ "$1" = "true" ]; then
-    echo_custom "GEM:" "Use local Sage gem located at: $sage_repo_path"
-    bundle config --local disable_local_branch_check true
-    bundle config --local local.Sage $sage_repo_path
-
     echo_custom "FRONTEND PACKAGE:" "Use the local Sage frontend package located at: $sage_repo_path"
     (cd $sage_repo_path; yarn link)
     yarn link sage
 
-    bundle_and_yarn
+    yarn_install
     echo_custom "Now Using..." "LOCAL SAGE ✅"
   fi;
 
-  show_status_of_gem_and_package
+  show_status_of_package
 
 elif [ "$1" = "status" ]; then
-    show_status_of_gem_and_package
+    show_status_of_package
 else
   echo_custom "Run inside the of root the Kajabi-Products repo:"
   echo "$ <PATH TO SAGE REPO>/bin/sage-local-link <BOOLEAN>"
