cca9c72dbcab62c4ed79c640901816633c516f1b
Andrew McIntee <contact@andrew.mn>
2020-09-04 12:22:14 -0400

    Adjust how we detect versions in docs site

--- config/initializers/load_sage_version.rb
+++ config/initializers/load_sage_version.rb
@@ -3,9 +3,6 @@ frontend_package_contents = JSON.parse(
                                 File.join(Rails.root, 'package.json')
                               )
                             )
-version_rails = SageRails::VERSION
-version_frontend = frontend_package_contents["version"]
-version_warning = (version_frontend == version_rails) ? ""  : "WARNING – Sage Rails & Sage Frontend don't have matching version numbers:&nbsp;&nbsp;"
-
-$SAGE_VERSION = "#{version_warning}📦 Package: v#{version_frontend}&nbsp;&nbsp;&nbsp;🧰 Gem: v#{version_rails}"
-$SAGE_VERSION_URL = "https://github.com/Kajabi/sage/releases/tag/v#{version_frontend}"
+$SAGE_VERSION_GEM = SageRails::VERSION
+$SAGE_VERSION_FRONTEND = frontend_package_contents["version"]
+$SAGE_VERSION_URL = "https://github.com/Kajabi/sage/releases/tag/v#{$SAGE_VERSION_FRONTEND}"
