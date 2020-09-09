cc9f5f4c74a416c98d0e845407b6f1decdfe4f89
Andrew McIntee <contact@andrew.mn>
2020-09-09 10:28:21 -0400

    Update Sage version detection display

--- config/initializers/load_sage_version.rb
+++ config/initializers/load_sage_version.rb
@@ -3,6 +3,11 @@ frontend_package_contents = JSON.parse(
                                 File.join(Rails.root, 'package.json')
                               )
                             )
+
 $SAGE_VERSION_GEM = SageRails::VERSION
 $SAGE_VERSION_FRONTEND = frontend_package_contents["version"]
-$SAGE_VERSION_URL = "https://github.com/Kajabi/sage/releases/tag/v#{$SAGE_VERSION_FRONTEND}"
+
+$SAGE_VERSION_GEM_URL = "https://github.com/Kajabi/sage/tree/v#{$SAGE_VERSION_GEM}/lib/sage_rails"
+$SAGE_VERSION_FRONTEND_URL = "https://github.com/Kajabi/sage/tree/v#{$SAGE_VERSION_FRONTEND}/lib/sage-frontend"
+
+$SAGE_RELEASE_URL = "https://github.com/Kajabi/sage/releases/tag/v#{$SAGE_VERSION_FRONTEND}"
