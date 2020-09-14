77e87e588b590a1f50208146114febef409c4dac
Phil Schanely <philschanely@gmail.com>
2020-09-14 11:35:33 -0400

    Add data card object

--- app/helpers/objects_helper.rb
+++ app/helpers/objects_helper.rb
@@ -67,6 +67,14 @@ module ObjectsHelper
         a11y: "todo"
       },
       {
+        title: "data_card",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", 
+        scss: "todo",
+        rails: "todo",
+        react: "todo",
+        a11y: "todo",
+      },
+      {
         title: "dropdown",
         description: "Sage dropdown",
         scss: "done",
