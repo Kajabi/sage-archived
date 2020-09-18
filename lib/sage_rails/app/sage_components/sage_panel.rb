cafa9cf38e6d6ad5b2ac2d9f92a09a29d17287f1
Andrew McIntee <contact@andrew.mn>
2020-09-18 18:30:25 -0400

    cleanup

--- lib/sage_rails/app/sage_components/sage_panel.rb
+++ lib/sage_rails/app/sage_components/sage_panel.rb
@@ -4,7 +4,6 @@ class SagePanel < SageComponent
   attr_accessor :size
   attr_accessor :style
   attr_accessor :title
-  attr_accessor :content_flush
 
   def sections
     %w(header_actions footer)
