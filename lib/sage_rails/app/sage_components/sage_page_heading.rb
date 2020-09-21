0605a1b4d65b11407955fd8223ed1110d36bcc93
Phillip Lovelace <phillip@pixelflips.com>
2020-09-21 12:08:03 -0700

    convert page heading to grid and adjust items

--- lib/sage_rails/app/sage_components/sage_page_heading.rb
+++ lib/sage_rails/app/sage_components/sage_page_heading.rb
@@ -1,6 +1,11 @@
 class SagePageHeading < SageComponent
   attr_accessor :title
-  attr_accessor :link_text
-  attr_accessor :link_url
+  attr_accessor :title_copy
   attr_accessor :help_link
+  attr_accessor :back_icon
+  attr_accessor :back_text
+  attr_accessor :back_url
+  attr_accessor :toolbar_items
+  attr_accessor :actions
+  attr_accessor :secondary_text
 end
