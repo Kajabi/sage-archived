b9b43225302eef08891a93e7c85be1b5e74655bd
Andrew McIntee <contact@andrew.mn>
2020-09-23 13:54:25 -0400

    SageLabel Rails, update Docs preview for SageDropdown, minor adjustments (#420)
    ### New
    - SageRails: SageLabel component
    
    ### Updates
    - Rename `CatalogCard` -> `CatalogItem`
    - SageLabels: Update to be 14pt type instead of 12pt (per new design updates)
    - SageButtons: Prevent from collapsing in height if an empty string is provided
    - SageDropdown: Rename `item[:name]` to `item[:value]` to match implementation of other components that accept item arrays.
    
    ### Docs
    - SageDropdown: component preview conversion

--- app/helpers/objects_helper.rb
+++ app/helpers/objects_helper.rb
@@ -85,7 +85,7 @@ module ObjectsHelper
       {
         title: "dropdown",
         description: "Sage dropdown",
-        use_legacy_html_code_source: true,
+        use_legacy_html_code_source: false,
         scss: "done",
         docs: "done",
         rails: "done",
@@ -240,8 +240,8 @@ module ObjectsHelper
         a11y: "todo"
       },
       {
-        title: "catalog_card",
-        description: "The Catalog Card displays Products, Offers, and other resources with a large amount of detail to display in list form.",
+        title: "catalog_item",
+        description: "The Catalog Item displays Products, Offers, and other resources with a large amount of detail to display in list form.",
         use_legacy_html_code_source: false,
         scss: "done",
         docs: "done",
