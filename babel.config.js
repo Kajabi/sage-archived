838a975581ccc0ba0fcbf17b44f56f4cca2d497a
Shane Smith <voodoogq@gmail.com>
2020-09-23 11:38:52 -0500

    Adding builtins

--- babel.config.js
+++ babel.config.js
@@ -39,6 +39,7 @@ module.exports = function (api) {
         '@babel/preset-react',
         {
           development: isDevelopmentEnv || isTestEnv,
+          useBuiltIns: true
         }
       ]
     ].filter(Boolean),
