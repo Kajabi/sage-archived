10d9dd530d671bfd4e212cabaf0a5e3c1190e11f
Shane Smith <voodoogq@gmail.com>
2020-09-25 10:37:15 -0500

    Working through main configuration of docs site on mono-repo configuration

---
+++ docs/lib/sage-frontend/packs/docs.js
@@ -0,0 +1,10 @@
+// --- Webpacker Dependencies ---
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
+
+// --- Sage Docs Assets ----------
+require.context('../images/docs', true)
+require.context('../images/system', true)
+import '../../../../stylesheets/docs/index.scss'
+import '../javascript/system/index'
+import '../javascript/docs/index'
---
+++ docs/lib/sage-frontend/packs/system.js
@@ -0,0 +1,8 @@
+// --- Webpacker Dependencies ---
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
+
+// --- Sage System Assets -------
+require.context('../images/system', true)
+import '../../../../docs/stylesheets/system/index.scss'
+import '../javascript/system/index'
