3e787a0173005d496cef17d28fbba0c97f58a6c3
Shane Smith <voodoogq@gmail.com>
2020-09-25 15:18:59 -0500

    Cache Directory

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,7 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
+            cacheDirectory: true,
             presets: ['@babel/preset-env', '@babel/preset-react']
           }
         }
