6d72d7e28ac668ecc02bd0b1a6d5a894fc788563
Shane Smith <voodoogq@gmail.com>
2020-09-25 15:08:03 -0500

    Updates to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
       {
         test: /\.scss$/,
         loaders: ["style-loader", "css-loader", "sass-loader"],
-        include: path.resolve(__dirname, '../')
+        include: path.resolve(__dirname, 'assets/stylesheets')
       }
     ],
   },
