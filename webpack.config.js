2e9b7c1a48726ff9d172e81b83b2e4cfc9916a7f
Shane Smith <voodoogq@gmail.com>
2020-09-28 13:07:32 -0500

    Container complete. Adding main.css to dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,22 @@
 // webpack.config.js
 var path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
-  entry: './assets/javascript/index.js',
+  entry: {
+    main: [
+      './assets/javascript/index.js',
+      './assets/stylesheets/system/index.scss'
+    ],
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
+    filename: '[name].js',
     libraryTarget: 'commonjs2'
   },
+  plugins: [new MiniCssExtractPlugin({
+    filename: '[name].css'
+  })],
   module: {
     rules: [
       {
@@ -22,23 +31,40 @@ module.exports = {
         }
       },
       {
-        test: /\.scss$/,
+        test: /\.?(s)css$/,
         use: [
-          { loader: 'css-loader' },
-          { loader: 'resolve-url-loader' },
           {
-            loader: 'sass-loader',
+            loader: MiniCssExtractPlugin.loader,
             options: {
               sourceMap: true,
               sourceMapContents: false
-            }
-          }
+            },
+          },
+          'css-loader',
+          'sass-loader'
         ],
-        include: path.resolve(__dirname, 'assets/stylesheets')
+        include: [
+          path.resolve(__dirname, 'assets/stylesheets'),
+          path.resolve(__dirname, 'assets/fonts')
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf)$/,
+        loader: 'url-loader',
+        options: {
+          limit: Infinity
+        }
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        loader: 'url-loader?limit=100000&name=./images/[name].[ext]',
+        options: {
+          limit: Infinity
+        }
       }
-    ],
+    ]
   },
   resolve: {
-    extensions: ['.js', '.jsx', '.css', '.scss', '.sass']
+    extensions: ['.js', '.jsx', '.css', '.scss', '.sass', '.woff', '.woff2', '.ttf', '.otf', '.svg']
   }
 }
