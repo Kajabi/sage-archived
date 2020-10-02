ab68a3627c8cc523c45cc339f87e327fa91f8b00
Phil Schanely <philschanely@gmail.com>
2020-10-02 10:52:27 -0400

    Update choice to include icon and arrow variations

--- lib/sage_rails/app/sage_components/sage_choice.rb
+++ lib/sage_rails/app/sage_components/sage_choice.rb
@@ -1,6 +1,9 @@
 class SageChoice < SageComponent
   attr_accessor :target
   attr_accessor :text
+  attr_accessor :type
+  attr_accessor :icon
   attr_accessor :subtext
   attr_accessor :active
+  attr_accessor :align_center
 end
