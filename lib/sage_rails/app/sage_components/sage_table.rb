2b39e17714499e7f076f3918dee9f132bbf085e4
Phil Schanely <philschanely@gmail.com>
2020-10-09 12:52:51 -0400

    WIP on rails table component

---
+++ lib/sage_rails/app/sage_components/sage_table.rb
@@ -0,0 +1,10 @@
+class SageTable < SageComponent
+  attr_accessor :caption
+  attr_accessor :responsive
+  attr_accessor :striped
+  attr_accessor :selectable
+  attr_accessor :condensed
+  attr_accessor :sortable
+  attr_accessor :headers
+  attr_accessor :rows
+end
