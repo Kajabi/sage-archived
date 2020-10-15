76d325bc0cffda9187dc5dff0e2ad595fbe8cc4c
Phillip Lovelace <phillip@pixelflips.com>
2020-10-15 13:05:48 -0700

    Alert Updates (#450)
    * updated markup
    
    * adjusted component properties
    
    * adjusted preview and styling
    
    * adjusted markup structure (hattip phil.s)
    
    * adjusted styles

--- lib/sage_rails/app/sage_components/sage_alert.rb
+++ lib/sage_rails/app/sage_components/sage_alert.rb
@@ -4,4 +4,8 @@ class SageAlert < SageComponent
   attr_accessor :title
   attr_accessor :desc
   attr_accessor :dismissable
+  attr_accessor :raised
+  def sections
+    %w(alert_actions)
+  end
 end
