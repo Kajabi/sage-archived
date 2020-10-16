5a92376274099154be31e9dbd2285c84212b91ba
Phil Schanely <philschanely@gmail.com>
2020-10-16 15:03:10 -0400

    Update props

--- lib/sage_rails/app/sage_components/sage_button.rb
+++ lib/sage_rails/app/sage_components/sage_button.rb
@@ -1,14 +1,14 @@
 class SageButton < SageComponent
+  attr_accessor :align
+  attr_accessor :attributes
+  attr_accessor :css_classes
+  attr_accessor :disabled
+  attr_accessor :full_width
+  attr_accessor :icon
+  attr_accessor :no_shadow
+  attr_accessor :raised
   attr_accessor :size
   attr_accessor :style
-  attr_accessor :align
-  attr_accessor :icon
-  attr_accessor :disabled
-  attr_accessor :attributes
-  attr_accessor :value
-  attr_accessor :full_width
-  attr_accessor :css_classes
   attr_accessor :subtle
-  attr_accessor :raised
-  attr_accessor :no_shadow
+  attr_accessor :value
 end
