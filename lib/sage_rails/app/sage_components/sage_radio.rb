b130b83c2d108eaccd7fd1d75c1fe265a3431080
Quinton Jason <quinton.jason@gmail.com>
2020-10-20 16:55:26 -0500

    Align Radio to spec and add Sage Rails Radio component (#462)
    * Align Radio to spec and add Sage Rails component
    
    * updated docs
    
    * moved variable location
    
    * linter
    
    * updated to demo code

---
+++ lib/sage_rails/app/sage_components/sage_radio.rb
@@ -0,0 +1,10 @@
+class SageRadio < SageComponent
+  attr_accessor :has_error
+  attr_accessor :id
+  attr_accessor :label_text
+  attr_accessor :checked
+  attr_accessor :disabled
+  attr_accessor :name
+  attr_accessor :required
+  attr_accessor :value
+end
