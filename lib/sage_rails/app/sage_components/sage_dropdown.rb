44a51efcd5e315b24ceee0df3ec8afcd81adf03f
Phil Schanely <philschanely@gmail.com>
2020-10-21 17:24:18 -0400

    WIP on toolbar

--- lib/sage_rails/app/sage_components/sage_dropdown.rb
+++ lib/sage_rails/app/sage_components/sage_dropdown.rb
@@ -1,6 +1,7 @@
 class SageDropdown < SageComponent
   attr_accessor :id
   attr_accessor :align
+  attr_accessor :contained
   attr_accessor :items
   attr_accessor :trigger_type
   attr_accessor :search
