488c0de92f3834b7223c6230bfca041e80d8e0fc
Phil Schanely <philschanely@gmail.com>
2020-10-23 09:02:05 -0400

    Dropdown improvements

--- lib/sage_rails/app/sage_components/sage_dropdown.rb
+++ lib/sage_rails/app/sage_components/sage_dropdown.rb
@@ -1,6 +1,7 @@
 class SageDropdown < SageComponent
   attr_accessor :id
   attr_accessor :align
+  attr_accessor :css_classes
   attr_accessor :contained
   attr_accessor :customized
   attr_accessor :custom_modifier
