4d93574264522bcd79725811d960b3b946532ec3
Phil Schanely <philschanely@gmail.com>
2020-10-23 09:13:53 -0400

    Wrap up panel controls build

--- lib/sage_rails/app/sage_components/sage_panel_controls.rb
+++ lib/sage_rails/app/sage_components/sage_panel_controls.rb
@@ -1,8 +1,11 @@
 class SagePanelControls < SageComponent
-  attr_accessor :show_expand_contract
-  attr_accessor :show_mini_pagination
+  attr_accessor :bulk_action_items
+  attr_accessor :show_expand_collapse
   attr_accessor :show_bulk_actions
+  attr_accessor :show_pagination
   attr_accessor :show_sort
+  attr_accessor :sort_items
+  attr_accessor :target
 
   def sections
     %w(panel_controls_toolbar panel_controls_tabs)
