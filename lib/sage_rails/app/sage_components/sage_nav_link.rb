9c71553e6efebde1bde13a8cdb535a59c38c972f
cmcfadzean <court@kajabi.com>
2020-10-27 12:50:11 -0700

    link icons and nesting

--- lib/sage_rails/app/sage_components/sage_nav_link.rb
+++ lib/sage_rails/app/sage_components/sage_nav_link.rb
@@ -2,6 +2,7 @@ class SageNavLink < SageComponent
   attr_accessor :link
   attr_accessor :text
   attr_accessor :type
+  attr_accessor :icon
   attr_accessor :no_active
   attr_accessor :method
 end
