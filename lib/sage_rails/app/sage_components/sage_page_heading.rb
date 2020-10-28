c8592c59b221776e43fe743e1832a5c103e84810
Quinton Jason <quinton.jason@gmail.com>
2020-10-28 08:48:04 -0500

    Added Sage Rails Popover

--- lib/sage_rails/app/sage_components/sage_page_heading.rb
+++ lib/sage_rails/app/sage_components/sage_page_heading.rb
@@ -3,10 +3,14 @@ class SagePageHeading < SageComponent
   attr_accessor :link_text
   attr_accessor :link_url
   attr_accessor :help_link
+  attr_accessor :help_title
+  attr_accessor :help_content
+  attr_accessor :help_content_link
+  attr_accessor :help_content_link_text
   attr_accessor :secondary_text
 
   def sections
-    %w(breadcrumbs page_heading_toolbar page_heading_actions page_heading_intro)
+    %w(breadcrumbs page_heading_toolbar page_heading_actions page_heading_intro help_content)
   end
 
 end
