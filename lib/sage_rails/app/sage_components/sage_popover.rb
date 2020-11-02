fb76503c248588fe810dff29234bfc3e8760140e
Quinton Jason <quinton.jason@gmail.com>
2020-11-02 14:03:34 -0600

    updated per review

--- lib/sage_rails/app/sage_components/sage_popover.rb
+++ lib/sage_rails/app/sage_components/sage_popover.rb
@@ -2,7 +2,7 @@ class SagePopover < SageComponent
   attr_accessor :id
   attr_accessor :title
   attr_accessor :body
-  attr_accessor :content_link
-  attr_accessor :content_link_text
+  attr_accessor :url_link
+  attr_accessor :link_text
 end
   
