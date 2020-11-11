677c8a7f0184d5697433d045bb5bc71b28aa15be
Quinton Jason <quinton.jason@gmail.com>
2020-11-11 09:59:29 -0600

    styled the tags

--- lib/sage_rails/app/sage_components/sage_label.rb
+++ lib/sage_rails/app/sage_components/sage_label.rb
@@ -1,7 +1,6 @@
 class SageLabel < SageComponent
   attr_accessor :color
   attr_accessor :style
-  attr_accessor :interactive
   attr_accessor :icon
   attr_accessor :value
   attr_accessor :html_tag
